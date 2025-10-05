d827401e301a14b252b1f94c41d56d8987bb6abf
Renato Henrique Silva <re_hsilva@hotmail.com>
2025-10-04 22:46:56 -0300

    Ajustes na estrutura inicial

---
+++ template/backend/src/Ambev.DeveloperEvaluation.WebApi/Mappings/GetUserResponseProfile.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Application.Users.CreateUser;
+using Ambev.DeveloperEvaluation.Application.Users.GetUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.GetUser;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings;
+
+public class GetUserResponseProfile : Profile
+{
+    public GetUserResponseProfile()
+    {
+        CreateMap<GetUserResult, GetUserResponse>();
+    }
+}
